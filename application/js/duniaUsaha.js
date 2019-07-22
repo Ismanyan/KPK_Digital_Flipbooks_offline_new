2dbb8734224e0bf3c12a6ee2e9767a2592e3064c
Bilkis Ismail <bilkisismail007@gmail.com>
2019-07-22 11:30:11 +0700

    Add new book

--- application/js/duniaUsaha.js
+++ application/js/duniaUsaha.js
@@ -309,4 +309,35 @@ $(document).ready(function () {
     // SHARING
     btnShare : {enabled: false}
   });
+
+  // R-PanduanCegahKorupsi
+  $(".R-PanduanCegahKorupsi").flipBook({
+    pdfUrl:"../output_pdf/Dunia Usaha/Panduan cegah korupsi/Panduan cegah korupsi.pdf",
+    lightBox:true,
+    lightBoxFullscreen:true,
+    layout:3,
+    currentPage:{vAlign:"bottom", hAlign:"left"},
+    // BTN SETTING
+    btnToc : {
+      enabled: true,
+      title: "Table of content",
+      icon: "fa-list-ol",
+      hideOnMobile: true
+    },
+    btnDownloadPdf : {
+      forceDownload: true,
+      icon: "fa-download"
+    },
+    btnPrint : {hideOnMobile:true},
+    btnDownloadPages : {enabled: false},
+    btnColor:'#ff2e2e',
+    sideBtnColor:'#ff2e2e',
+    sideBtnSize:60,
+    sideBtnBackground:"rgba(0,0,0,.7)",
+    sideBtnRadius:60,
+    btnSound:{vAlign:"top", hAlign:"left"},
+    btnAutoplay:{vAlign:"top", hAlign:"left"},
+    // SHARING
+    btnShare : {enabled: false}
+  });
 });
